09fa717541054eaa6ac9a152f9e3d35c3fd6bc01
Jaimejourney <jaimejourney@163.com>
2018-10-31 20:18:30 -0700

    JS

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$(".d").click(function() {
+		$("html, body").animate({
+			scrollTop: 750
+		}, 1000);
+	});
+});
+
+$(document).ready(function() {
+	$(".da").click(function() {
+		$(".side").toggle();
+	});
+});
+
+$(document).ready(function() {
+	$(".x").click(function() {
+		$(".side").toggle();
+	});
+});
+
